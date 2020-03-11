af0ef6e8cf603b33782ef2f5d34fce60ab80e3f7
Matze312 <m.piefke@t-online.de>
2020-03-11 15:21:56 +0100

    Ergänzung des AbwehrArrays

--- main.cpp
+++ main.cpp
@@ -4,11 +4,18 @@ using namespace std;
 
 int main()
 {
-    string tor[3] = { "Neuer", "Trapp", "Ter Stegen"};
+    string tor[3] = { "Kolke", "Sebald", "Voll"};
 
     for (unsigned int i = 0; i < tor->size(); i++) {
 
         cout <<tor[i] << endl;
+    }
+
+    string abwehr[7] = { "Riedel", "Straith", "Sonnenberg", "Reinthaler", "Rieble", "Ahlschwede", "Neidhart"};
+
+    for(unsigned int a = 0; a < abwehr->size(); a++)
+    {
+        cout <<abwehr[a] << endl;
     }
     return 0;
 }
